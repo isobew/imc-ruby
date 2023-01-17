ef8d6573e06bd8c990bd0af2aa6e7f9e520927d2
Isabella Firmino <isabella.mfbr@gmail.com>
2023-01-17 17:58:05 -0400

    Adding JWT

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+    before_action :authenticate_request
+
+    private
+
+        def authenticate_request
+            header = request.headers["Authorization"]
+            header = header.split(" ").last if header
+            @decoded = JWT.decode(header)
+            @current_imc = Imc.find(decoded[:imc_id])
+        end
 end
--- app/controllers/imc_controller.rb
+++ app/controllers/imc_controller.rb
@@ -1,14 +1,15 @@
 class ImcController < ApplicationController
+  skip_before_action :authenticate_request, only: %i[ create ]
   before_action :set_imc, only: %i[ show update destroy ]
 
   # POST /imc
   def create
     @imc = Imc.new(imc_params)
-    
+
     if @imc.save
       imc = (@imc.weight / (@imc.height * @imc.height)).round(2)
       @imc.imc = imc
-
+      
       case @imc.imc
       when ..18.5
         @imc.classification = "Magreza"
@@ -26,17 +27,18 @@ class ImcController < ApplicationController
         @imc.classification = "Obesidade grave"
         @imc.obesity = "III"
       end
-
+      
       render json: {
-        imc: @imc.imc, 
-        classification: @imc.classification, 
-        obesity: @imc.obesity
-      }, 
-      status: :created, 
-      location: @imc
-    else
-      render json: @imc.errors, status: :unprocessable_entity
-    end
+          imc: @imc.imc, 
+          classification: @imc.classification, 
+          obesity: @imc.obesity,
+        }, 
+        status: :created, 
+        location: @imc
+      else
+        render json: @imc.errors, status: :unprocessable_entity
+      end
+    
   end
 
   private
@@ -47,6 +49,6 @@ class ImcController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def imc_params
-      params.require(:imc).permit(:height, :weight, :imc, :classification, :obesity)
+      params.require(:imc).permit(:height, :weight)
     end
 end
